04389b9d47c9efbea4b2782e2fe75e01155c797b
Paul Obalonye <Paul.Obalonye@gmail.com>
2020-07-29 18:36:38 -0400

    updated the person table with employees record

---
+++ AdventureWorks/Person/Tables/employees.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [person].[employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Address] NCHAR(100) NULL, 
+    [Phone] NCHAR(10) NULL, 
+    [Sex] NCHAR(6) NULL
+)
