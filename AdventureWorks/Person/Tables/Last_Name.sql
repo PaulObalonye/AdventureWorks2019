731088664727edb070b3da7e064945e47cc856e9
Paul Obalonye <Paul.Obalonye@gmail.com>
2020-07-29 21:25:19 -0400

    Added Last table

---
+++ AdventureWorks/Person/Tables/Last_Name.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [First_Name] NCHAR(10) NULL, 
+    [Last_Name] NCHAR(10) NULL, 
+    [Phone_Number] NCHAR(10) NULL, 
+    [Age] NCHAR(10) NULL, 
+    [SSN] NCHAR(10) NULL, 
+    [DOB] NCHAR(10) NULL
+)
